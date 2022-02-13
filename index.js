39671049dc95703a13854dcafe4ba21ff929d2fb
Denis Tsibaev <denis.tsibaev@gmail.com>
2022-02-13 23:08:41 +0700

    gitignore

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const date = new Date();
 
+console.log("Welcome to the Internet!");
+console.log("Welcome to the Gorkiy Park");
 console.log(date.getFullYear());
-console.log("Welcome to the internet!");
