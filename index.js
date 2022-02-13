00454a9b24bbaafdc55cc0bd4f6dbdff03359f66
Denis Tsibaev <denis.tsibaev@gmail.com>
2022-02-13 23:44:22 +0700

    require

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const date = new Date();
 
-console.log("Welcome to the Internet!");
-console.log("Welcome to the Gorkiy Park");
-console.log(date.getFullYear());
+// console.log("Welcome to the Internet!");
+// console.log("Welcome to the Gorkiy Park");
+// console.log(date.getFullYear());
+
+const users = require("./users");
+console.log(users);
+
+const { admins } = require("./users");
+console.log("admins: ", admins);
---
+++ users.js
@@ -0,0 +1,9 @@
+const admins = ["Alex", "Ustas", "Valera"];
+const clients = ["Tamara", "Anna", "Ira"];
+
+const users = {
+  admins,
+  clients,
+};
+
+module.exports = users;
